4afdf74b1daabd9bb9a3a6cc1bf9bdbc9f192f9a
Xandarel <aobordug@gmail.com>
2020-02-10 11:31:22 +0500

    Скобка решена

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace Calculator
             } while ((bracketsPosition = expression.Count(c => c == '(')) != 0);
             Console.ReadKey();
         }
-        static void DecisionBrackets(string text)//TODO: не нравится переделать
+        static double DecisionBrackets(string text)//TODO: не нравится переделать
         {
             Regex regex = new Regex(@"[+|-|*|/]");
             MatchCollection matchCollection = regex.Matches(text);
@@ -32,6 +32,31 @@ namespace Calculator
                 signs[positionInArray] = match.ToString();
                 positionInArray++;
             }
+
+            if (signs.Length == 1)
+                return DecisionSign(Convert.ToDouble(numbers[0]), Convert.ToDouble(numbers[1]), signs[0]);
+            else
+                for (positionInArray = 0; positionInArray < signs.Length; positionInArray++)
+                {
+                    if ((signs[positionInArray] == "+" || signs[positionInArray] == "-") &&
+                        (signs[positionInArray + 1] != "*" || signs[positionInArray + 1] != "*"))
+                    {
+                        numbers[positionInArray + 1] = DecisionSign(Convert.ToDouble(numbers[positionInArray]),
+                            Convert.ToDouble(numbers[positionInArray + 1]),
+                            signs[positionInArray]).ToString();
+                        numbers[positionInArray] = null;
+                        signs[positionInArray] = null;
+                    }
+                    else if (signs[positionInArray] == "*" || signs[positionInArray] == "/")
+                    {
+                        numbers[positionInArray + 1] = DecisionSign(Convert.ToDouble(numbers[positionInArray]),
+                            Convert.ToDouble(numbers[positionInArray + 1]),
+                            signs[positionInArray]).ToString();
+                        numbers[positionInArray] = null;
+                        signs[positionInArray] = null;
+                    }
+                        
+                }
         }
         static void FindBrackets(string text,Regex regex)
         {
@@ -41,9 +66,26 @@ namespace Calculator
                 string newText = DeleteBrackets(matches);
                 FindBrackets(newText, regex);
             }
-            DecisionBrackets(text);
+            var result=DecisionBrackets(text);//TODO: дописать замену выражения на новое
         }
 
         static string DeleteBrackets(Match match) => match.ToString().Substring(1, match.Length - 2);
+
+        static double DecisionSign(double s1,double s2, string sign)
+        {
+            switch (sign)//TODO: сделать тдельной функцией
+            {
+                case "+":
+                    return s1 + s2;
+                case "-":
+                    return s1 - s2;
+                case "*":
+                    return s1 * s2;
+                case "/":
+                    return s1 / s2;
+                default:
+                    return 0;
+            }
+        }
     }
 }
